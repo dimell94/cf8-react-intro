aee9d5c38f595e0622a113e3bceb7166e1d3a5ef
dimell94 <dim.ell.094@gmail.com>
2025-10-03 14:14:44 +0300

    feat: add layout, footer, header

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 // import ViteIntro from "./components/ViteIntro/ViteIntro.tsx";
 
 
-import ClassComponent from "./components/ClassComponent.tsx";
-import FunctionalComponent from "./components/FunctionalComponent.tsx";
-import ArrowFunctionalComponent from "./components/ArrowFunctionalComponent.tsx";
-import ArrowFunctionalComponentWIthProp from "./components/ArrowFunctionalComponentWIthProp.tsx";
+// import ClassComponent from "./components/ClassComponent.tsx";
+// import FunctionalComponent from "./components/FunctionalComponent.tsx";
+// import ArrowFunctionalComponent from "./components/ArrowFunctionalComponent.tsx";
+// import ArrowFunctionalComponentWIthProp from "./components/ArrowFunctionalComponentWIthProp.tsx";
 import ArrowFunctionalComponentWIthPropType from "./components/ArrowFunctionalComponentWIthPropType.tsx";
-import Card from "./components/Card.tsx";
+// import Card from "./components/Card.tsx";
+import Layout from "./components/Layout.tsx";
 
 function App() {
 
@@ -14,20 +15,27 @@ function App() {
   return (
     <>
         {/*<ViteIntro />*/}
-        <ClassComponent />
-        <FunctionalComponent />
-        <ArrowFunctionalComponent />
-        <ArrowFunctionalComponentWIthProp title="is an arrow functional component with props" />
-        <ArrowFunctionalComponentWIthProp title="Title2" />
-        <ArrowFunctionalComponentWIthPropType title="is a functional component with 2 props" description=
-            "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid, et."
-        />
+        {/*<ClassComponent />*/}
+        {/*<FunctionalComponent />*/}
+        {/*<ArrowFunctionalComponent />*/}
+        {/*<ArrowFunctionalComponentWIthProp title="is an arrow functional component with props" />*/}
+        {/*<ArrowFunctionalComponentWIthProp title="Title2" />*/}
 
-        <Card title="Card">
-            <ArrowFunctionalComponentWIthPropType title="is a functional component with 2 props" description=
-                "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid, et."
-            />
-        </Card>
+
+        {/*<Card title="Card">*/}
+        {/*    <ArrowFunctionalComponentWIthPropType title="is a functional component with 2 props" description=*/}
+        {/*        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid, et."*/}
+        {/*    />*/}
+        {/*</Card>*/}
+
+             <Layout>
+                 <ArrowFunctionalComponentWIthPropType title="is a functional component with 2 props" description=
+                     "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid, et."
+                 />
+
+             </Layout>
+
+
     </>
   )
 }
