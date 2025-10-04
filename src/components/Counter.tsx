04e2063e96f7c15f6444334835ec1b1241068d81
dimell94 <dim.ell.094@gmail.com>
2025-10-04 15:53:29 +0300

    feat: add counter button component for button

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,4 +1,5 @@
 import {useState} from "react";
+import CounterButton from "./CounterButton.tsx";
 
 const Counter = () => {
    const [count, setCount] = useState(0);
@@ -19,18 +20,31 @@ const Counter = () => {
         <>
             <h1 className="text-center my-12">Count is <strong>{count}</strong></h1>
             <div className="text-center space-x-4">
-                <button onClick= {increaseCount} className="bg-cf-dark-gray text-white py-2 px-4">
-                    Increase
-                </button>
-                <button onClick= {decreaseCount} disabled={count <= 0} className="bg-cf-dark-gray disabled:bg-cf-gray text-white py-2 px-4">
-                    Decrease
-                </button>
+                {/*<button onClick= {increaseCount} className="bg-cf-dark-gray text-white py-2 px-4">*/}
+                {/*    Increase*/}
+                {/*</button>*/}
+
+                <CounterButton onClick={increaseCount} label="Increase" addClasses="bg-cf-dark-gray" />
+
+            {/*    <button onClick= {decreaseCount} disabled={count <= 0} className="bg-cf-dark-gray disabled:bg-cf-gray text-white py-2 px-4">*/}
+            {/*        Decrease*/}
+            {/*    </button>*/}
+
+
+                <CounterButton onClick={decreaseCount} label="Decrease" disabled={count === 0} addClasses="bg-cf-dark-gray disabled: disabled:bg-cf-gray" />
+
             </div>
+
             <div className="text-center mt-4">
-                <button onClick= {resetCount} disabled={count === 0} className="bg-cf-dark-gray disabled:bg-cf-gray text-white py-2 px-4">
-                    Reset
-                </button>
+            {/*    <button onClick= {resetCount} disabled={count === 0} className="bg-cf-dark-gray disabled:bg-cf-gray text-white py-2 px-4">*/}
+            {/*        Reset*/}
+            {/*    </button>*/}
+
+                <CounterButton onClick={resetCount} label="Reset" disabled={count === 0} addClasses="bg-cf-dark-red disabled:bg-red-500" />
+
+
             </div>
+
 
         </>
     )
