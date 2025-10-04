f968d1a8a10a24ee3970ea00c35e5e70efbd9ca3
dimell94 <dim.ell.094@gmail.com>
2025-10-04 16:11:03 +0300

    feat: add name changer

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,8 @@
 // import ArrowFunctionalComponentWIthPropType from "./components/ArrowFunctionalComponentWIthPropType.tsx";
 // import Card from "./components/Card.tsx";
 import Layout from "./components/Layout.tsx";
-import Counter from "./components/Counter.tsx";
+// import Counter from "./components/Counter.tsx";
+import NameChanger from "./components/NameChanger.tsx";
 // import FunctionalComponentWithState from "./components/FunctionalComponentWithState.tsx";
 // import ClassComponentWithState from "./components/ClassComponentWithState.tsx";
 
@@ -38,7 +39,10 @@ function App() {
 
                  {/*<FunctionalComponentWithState />*/}
                  {/*<ClassComponentWithState />*/}
-                 <Counter />
+
+                 {/*<Counter />*/}
+
+                 <NameChanger />
 
              </Layout>
 
